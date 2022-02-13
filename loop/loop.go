a6bf25055860cd36728a8634cdcb0303ff1e4435
Shahab Eshaghi <shahabeshaghi67@gmail.com>
2022-02-13 16:24:42 +0330

    feat(loop): added infinite loop

---
+++ loop/loop.go
@@ -0,0 +1,44 @@
+package loop
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+func InfiniteLoop(ctx context.Context, name string, f func(context.CancelFunc) error, reDoDuration, failDuration time.Duration) context.Context {
+	go func() {
+		DoTasKWithRetry(ctx, name, f, failDuration)
+		select {
+		case <-ctx.Done():
+			logrus.Debug(fmt.Sprintf("exit infinite loop: %s", name))
+			return
+		case <-time.After(reDoDuration):
+		}
+	}()
+	return ctx
+}
+
+func DoTasKWithRetry(c context.Context, name string, f func(context.CancelFunc) error, failDuration time.Duration) {
+	ctx, cl := context.WithCancel(c)
+	defer func() {
+		e := recover()
+		if e != nil {
+			logrus.Errorf(fmt.Sprintf("%s failed", name))
+		}
+	}()
+	for {
+		err := f(cl)
+		if err == nil {
+			return
+		}
+		select {
+		case <-ctx.Done():
+			logrus.Debug(fmt.Sprintf("exit infinite loop: %s", name))
+			return
+		case <-time.After(failDuration):
+		}
+	}
+}
