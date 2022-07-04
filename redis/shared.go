be6cc1a370564f666dd890103e0b60bc9c7f64e5
Mehdi Makki <m.makki@snapp.express>
2022-07-04 22:07:42 +0430

    refactor redis fetch function

--- redis/shared.go
+++ redis/shared.go
@@ -10,6 +10,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type dataFetcher[K any] func(ctx context.Context) (K, error)
+
 // this is used when multi instances of a code try to use shared cache on redis
 // and result fetch function has too much load expensive
 // it will return the same result for all of them
@@ -18,7 +20,7 @@ import (
 // if it was the case it will not try to fetch
 // but if no one of instances has tried to fetch the result it start to fetch and set a waitKey
 // too inform others
-func SharedFetch[K any](ctx context.Context, client redis.Cmdable, key string, t, retryT time.Duration, retry int, f func(ctx context.Context) (K, error)) (K, error) {
+func SharedFetch[K any](ctx context.Context, client redis.Cmdable, key string, t, retryT time.Duration, retry int, f dataFetcher[K]) (K, error) {
 	var res K
 	redisRes := client.Get(ctx, key)
 	go func() { _ = Fetch(ctx, client, key, t, retryT, retry, f) }()
@@ -37,39 +39,71 @@ func SharedFetch[K any](ctx context.Context, client redis.Cmdable, key string, t
 	return res, nil
 }
 
-func Fetch[K any](ctx context.Context, client redis.Cmdable, key string, t, retryT time.Duration, retry int, f func(ctx context.Context) (K, error)) error {
-	waitKey := fmt.Sprintf("%s:wait", key)
-	cmd := client.SetNX(ctx, waitKey, true, retryT)
-	if val, err := cmd.Result(); err != nil || val == false {
+func Fetch[K any](ctx context.Context, client redis.Cmdable, key string, t, retryT time.Duration, retry int, f dataFetcher[K]) error {
+	err := lock(ctx, client, key, retryT)
+	if err != nil {
 		logrus.Infof("goxy:SharedFetch:%s-lock:%v", key, err)
 		return err
 	}
-	var res K
-	var err error
-	for i := 0; i < retry; i++ {
-		res, err = f(ctx)
-		if err == nil {
-			break
-		}
-	}
+	res, err := fetchData(ctx, f, retry)
 	if err != nil {
-		cmd := client.SetNX(ctx, waitKey, false, retryT)
-		if val, lErr := cmd.Result(); err != nil || val == true {
-			logrus.Errorf("goxy:SharedFetch:%s-unlock:%s", key, lErr)
+		if e := unlock(ctx, client, key, retryT); e != nil {
+			logrus.Errorf("goxy:SharedFetch:%s-unlock:%s", key, e)
 		}
 		logrus.Errorf("goxy:SharedFetch:fetch(after %d times):%s:%s", retry, key, err.Error())
 		return err
 	}
-	redisSetVal, err := goxy.Encode(res)
+	err = setInredis(ctx, client, key, t, res)
 	if err != nil {
-		logrus.Errorf("goxy:SharedFetch:%s-encode:%s", key, err.Error())
+		logrus.Error(err)
+		return err
+	}
+	logrus.Infof("goxy:SharedFetch:Set:%s:Successfully", key)
+	return nil
+}
+
+func lock(ctx context.Context, client redis.Cmdable, key string, t time.Duration) error {
+	return changeLockstatus(ctx, client, key, t, true)
+}
+
+func unlock(ctx context.Context, client redis.Cmdable, key string, t time.Duration) error {
+	return changeLockstatus(ctx, client, key, t, false)
+}
+
+func changeLockstatus(ctx context.Context, client redis.Cmdable, key string, t time.Duration, status bool) error {
+	k := lockingKey(key)
+	cmd := client.SetNX(ctx, k, status, t)
+	if val, err := cmd.Result(); err != nil || val == false {
+		return err
+	}
+	return nil
+}
+
+func fetchData[K any](ctx context.Context, fun func(ctx context.Context) (K, error), retry int) (res K, err error) {
+	for i := 0; i < retry; i++ {
+		res, err = fun(ctx)
+		if err != nil {
+			continue
+		}
+		return
+	}
+	return
+}
+
+func lockingKey(k string) string {
+	return fmt.Sprintf("%s:wait", k)
+}
+
+func setInredis[K any](ctx context.Context, client redis.Cmdable, key string, t time.Duration, data K) error {
+	redisSetVal, err := goxy.Encode(data)
+	if err != nil {
+		err = fmt.Errorf("goxy:SharedFetch:%s-encode:%s", key, err.Error())
 		return err
 	}
 	err = client.Set(ctx, key, redisSetVal, t).Err()
 	if err != nil {
-		logrus.Errorf("goxy:SharedFetch:Set:%s:%s", key, err.Error())
+		err = fmt.Errorf("goxy:SharedFetch:Set:%s:%s", key, err.Error())
 		return err
 	}
-	logrus.Infof("goxy:SharedFetch:Set:%s:Successfully", key)
 	return nil
 }
